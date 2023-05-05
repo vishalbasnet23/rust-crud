0695304a84e144b7f86e04a522ec18bc61fa6ddb
Bishal Basnet <vishalbasnet23@gmail.com>
2023-05-05 14:50:59 +0545

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use postgres::{ Client, NoTls };
+use postgres::Error as PostgresError;
+use std::net::{ TcpListener, TcpStream };
+use std::io::{ Read, Write };
+use std::env;
+
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: Option<i32>,
+    name: String,
+    email: String,
+}
